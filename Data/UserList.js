1b703608c79137afb2f124f53ca84e9222ee1032
Prabhu Raj Aranvoyal Rajan <prabhuraj.aranvoyalrajan@siriuscom.com>
2022-01-28 11:07:10 +0530

    Boilerplate with data and packages setup

---
+++ Data/UserList.js
@@ -0,0 +1,74 @@
+const UserList = [
+  {
+    id: 1,
+    user_name: 'mkatte0',
+    user_email: 'htwinn0@pcworld.com',
+    age: 32,
+    'nationality ': 'Bolivia',
+  },
+  {
+    id: 2,
+    user_name: 'dsandys1',
+    user_email: 'ahearnden1@ovh.net',
+    age: 24,
+    'nationality ': 'Philippines',
+  },
+  {
+    id: 3,
+    user_name: 'ibeever2',
+    user_email: 'ctinner2@example.com',
+    age: 29,
+    'nationality ': 'China',
+  },
+  {
+    id: 4,
+    user_name: 'hgranham3',
+    user_email: 'ddonovan3@accuweather.com',
+    age: 44,
+    'nationality ': 'Sweden',
+  },
+  {
+    id: 5,
+    user_name: 'owindsor4',
+    user_email: 'ahenriques4@friendfeed.com',
+    age: 46,
+    'nationality ': 'South Korea',
+  },
+  {
+    id: 6,
+    user_name: 'fstormonth5',
+    user_email: 'rfeaveryear5@examiner.com',
+    age: 23,
+    'nationality ': 'Ukraine',
+  },
+  {
+    id: 7,
+    user_name: 'umacdermott6',
+    user_email: 'tvodden6@apple.com',
+    age: 32,
+    'nationality ': 'China',
+  },
+  {
+    id: 8,
+    user_name: 'hblizard7',
+    user_email: 'gmenat7@sfgate.com',
+    age: 34,
+    'nationality ': 'Philippines',
+  },
+  {
+    id: 9,
+    user_name: 'ccicccitti8',
+    user_email: 'efellows8@theglobeandmail.com',
+    age: 32,
+    'nationality ': 'China',
+  },
+  {
+    id: 10,
+    user_name: 'dwhellams9',
+    user_email: 'hmitchenson9@cmu.edu',
+    age: 38,
+    'nationality ': 'Portugal',
+  },
+];
+
+module.exports = { UserList };
